8eea8efa1e1ccbc6aafbe386d623e4cb7fc75287
Ashvini Chaudhari <ashvinichaudhari9137@gmail.com>
2025-11-25 14:05:46 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-
+import Body from './components/Body';
 import './App.css'
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-800">
-      Tailwind Working 🎉
-    </h1>
+    <>
+    <Body />
+    </>
   );
 }
 
